ba5cd6ea02d130585d0e72801e1de2a67dd9b5f9
SabishiiKun <fabrizzioamegre@gmail.com>
2024-10-13 15:35:14 -0300

    e for your changes. Lines starting

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <MiComponente></MiComponente>
+    <div class="navbar-container">
+                <div class="navbar">
+                    <div class="logo">
+                        Sport
+                        <span>Fit</span>
+                    </div>
+                    <a href="">Home</a>
+                    <a href="">About Us</a>
+                    <a href="">Classes & Services</a>
+                    <a href="">Blog</a>
+                    <a href="">Contact</a>
+                </div>
+            </div>
+  </div>
+</template>
+
+<script>
+import MiComponente from './components/MiComponente.vue';
+ 
+export default {
+  name: 'App',
+  components: {
+    MiComponente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+@import "./assets/css/style.scss";
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="mi-componente">
+        <div class="content">
+            <div class="home">
+                <div class="banner-container">
+                    <div class="overlay-text">
+                        <div class="main-text">Get Fit With Us</div>
+                        <div class="sub-text">Join our fitness community today!</div>
+                            <div class="button-home">
+                                <div>
+                                    <a href="">Join Now</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MiComponente',
+};
+</script>
+
+<style scoped>
+</style>
